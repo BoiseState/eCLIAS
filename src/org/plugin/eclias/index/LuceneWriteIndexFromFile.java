60315cb84fa761014aa2fd0c5c7b7f435ec4619a
Vasanthgeethan Raju <vsanthgeethan.raju@gmail.com>
2019-11-22 21:09:21 -0700

    java file opens on clicking the table content

--- src/org/plugin/eclias/index/LuceneWriteIndexFromFile.java
+++ src/org/plugin/eclias/index/LuceneWriteIndexFromFile.java
@@ -47,8 +47,7 @@ import org.eclipse.jdt.core.IPackageFragment;
 import org.eclipse.jdt.core.IType;
 import org.eclipse.jdt.core.JavaCore;
 
-public class LuceneWriteIndexFromFile
-{
+public class LuceneWriteIndexFromFile {
 	static HashMap<IMethod, Method> methods = new HashMap<IMethod, Method>();
 	static HashMap<String, IMethod> methodMap = new HashMap<String, IMethod>();
 	static HashMap<String, IMethod> methodIDMap = new HashMap<String, IMethod>();
@@ -58,138 +57,134 @@ public class LuceneWriteIndexFromFile
 	private static Query query;
 	private static String projectsname;
 	private static String indexPath = "";
-	
-   public static class Score{
-	   
+
+	public static class Score {
+
 		float score;
 		IMethod method;
-		String packageName; 
+		String packageName;
 		String methodName;
-		IMethod method1;
-		
-		Score(float score, IMethod method, String packageName, String methodName, IMethod method1){
+		static IMethod ideEditor;
+		String className;
+
+		Score(float score, IMethod method, String packageName, String methodName, String className, IMethod ideEditor) {
 			this.score = score;
 			this.method = method;
-			this.packageName = packageName; 
+			this.packageName = packageName;
 			this.methodName = methodName;
-			this.method1 = method1; 
+			this.className = className;
+			this.ideEditor = ideEditor;
 		}
-		
-		public float getScore(){
+
+		public float getScore() {
 			return score;
 		}
-		
+
 		public IMethod getMethod() {
 			return method;
 		}
-		
+
 		public String getPackageName() {
 			return packageName;
 		}
-		
+
 		public String getMethodName() {
 			return methodName;
 		}
-		
-		public IMethod getMethod1() {
 
-			return method1;
+		public String getClassName() {
+			return className;
+		}
+
+		public static IMethod getEditor() {
+			return ideEditor;
 		}
 	}
 
-	public static void index() throws Exception
-	{
+	public static void index() throws Exception {
 		IWorkspace root = ResourcesPlugin.getWorkspace();
 		IProject[] allProjects = root.getRoot().getProjects();
 
 		for (IProject project : allProjects) {
-			if(project.isNatureEnabled("org.eclipse.jdt.core.javanature")) {
+			if (project.isNatureEnabled("org.eclipse.jdt.core.javanature")) {
 				IPackageFragment[] frags = JavaCore.create(project).getPackageFragments();
 				for (IPackageFragment frag : frags) {
 					for (ICompilationUnit unit : frag.getCompilationUnits()) {
 						for (IType type : unit.getAllTypes()) {
 							for (IMethod method : type.getMethods()) {
-								methodIDMap.put(
-										method.getHandleIdentifier(), //
+								methodIDMap.put(method.getHandleIdentifier(), //
 										method);
 							}
 						}
 					}
 				}
-				
+
 				// List the projects in the explorer tab
-				projectsname = project.toString(); 
+				projectsname = project.toString();
 				System.out.println(projectsname);
-				
+
 				// Creating a new directory to store indexed files
-				File newDirectory= new File("/Users/Vasanth/git/eCLIAS/indexedFiles/Example/" + projectsname);
+				File newDirectory = new File("/Users/Vasanth/git/eCLIAS/indexedFiles/Example/" + projectsname);
 				newDirectory.mkdirs();
-				
-				// Path of indexed files 
+
+				// Path of indexed files
 				System.out.println("assigning index path");
 				indexPath += "/Users/Vasanth/git/eCLIAS/indexedFiles/Example/" + projectsname;
-
-				
 
 				// analyzer with the default stop words
 //				Analyzer analyzer = new StandardAnalyzer();
 
-				
-
 				// IndexWriter writes new index files to the directory
-				//IndexWriter writer = new IndexWriter(dir, iwc);
+				// IndexWriter writer = new IndexWriter(dir, iwc);
 				// Its recursive method to iterate all files and directories
-				//indexDocs(writer, docDir);
-				
-				
+				// indexDocs(writer, docDir);
+
 				Job job = new Job("Lucene Indexing") {
 
 					@Override
 					protected IStatus run(IProgressMonitor monitor) {
-						
+
 						IndexWriter writer;
 						try {
-							
+
 							// org.apache.lucene.store.Directory instance
 							Directory dir = FSDirectory.open(Paths.get(indexPath));
-							
+
 							// IndexWriter Configuration
 							IndexWriterConfig iwc = new IndexWriterConfig(getAnalyzer());
 							iwc.setOpenMode(OpenMode.CREATE_OR_APPEND);
-							
+
 							// IndexWriter writes new index files to the directory
 							writer = new IndexWriter(dir, iwc);
-							
+
 							writer.deleteAll();
-							
-							monitor.beginTask("Building index",
-									methodIDMap.size());
-							
+
+							monitor.beginTask("Building index", methodIDMap.size());
+
 //							int count = 0;
-							
+
 //							System.out.println("methodIDMap: KeySet = " +methodIDMap.keySet()); //trying to print out what's the value
-							
+
 							for (String key : methodIDMap.keySet()) {
-								indexDoc(writer, methodIDMap.get(key)); 
+								indexDoc(writer, methodIDMap.get(key));
 								monitor.worked(1);
 //								System.out.println("document is:" +methodIDMap.get(key));								
 							}
 //								int check = writer.numDocs();
-								
+
 //								while(count<=check) {
 //									count ++;
 //										 }	
 //							}
 							monitor.done();
 							writer.close();
-							
+
 //							
 //							System.out.println("Count is:" +count);
 //							// print all info about writer
 //							System.out.println("writer is:" +writer.toString());
 //							System.out.println("Monitor is:" +monitor.toString());
 
-							
 //							Terms terms = null;
 //							int doccount = terms.getDocCount();
 //							System.out.println(doccount);
@@ -206,13 +201,11 @@ public class LuceneWriteIndexFromFile
 				job.setUser(true);
 				job.schedule();
 
-			}
-			else {
+			} else {
 				System.out.println("error - It's not a project");
-			}	
+			}
 		}
 	}
-
 
 	static void indexDoc(IndexWriter writer, IMethod method) {
 		Document doc = new Document();
@@ -224,7 +217,7 @@ public class LuceneWriteIndexFromFile
 //			System.out.println("source is:"+source);
 			if (source != null) {
 				source = IRUtil.splitAndMergeCamelCase(source);
-				
+
 				doc.add(new StringField("path", source, Field.Store.YES));
 				doc.add(new LongPoint("modified", lastModified));
 				doc.add(new TextField("contents", source, Store.YES));
@@ -238,15 +231,11 @@ public class LuceneWriteIndexFromFile
 	}
 
 	static CharArraySet getStopWords() {
-		String[] JAVA_STOP_WORDS = { "public", "private", "protected",
-				"interface", "abstract", "implements", "extends", "null",
-				"new", "switch", "case", "default", "synchronized", "do", "if",
-				"else", "break", "continue", "this", "assert", "for",
-				"instanceof", "transient", "final", "static", "void", "catch",
-				"try", "throws", "throw", "class", "finally", "return",
-				"const", "native", "super", "while", "import", "package",
-				"true", "false", "but", "does", "shouldn't", "aren't", "are",
-				"should", "such", "they", "how" };
+		String[] JAVA_STOP_WORDS = { "public", "private", "protected", "interface", "abstract", "implements", "extends",
+				"null", "new", "switch", "case", "default", "synchronized", "do", "if", "else", "break", "continue",
+				"this", "assert", "for", "instanceof", "transient", "final", "static", "void", "catch", "try", "throws",
+				"throw", "class", "finally", "return", "const", "native", "super", "while", "import", "package", "true",
+				"false", "but", "does", "shouldn't", "aren't", "are", "should", "such", "they", "how" };
 		HashSet<String> javaStopWords = new HashSet<String>();
 		javaStopWords.addAll(Arrays.asList(JAVA_STOP_WORDS));
 		CharArraySet allStopWords = new CharArraySet(javaStopWords, false);
@@ -260,48 +249,40 @@ public class LuceneWriteIndexFromFile
 		}
 		return analyzer;
 	}
-	
+
 	static IndexReader getIndexReader() {
 		return reader;
 	}
-	
+
 	static Integer getDocNum(IMethod iMethod) throws IOException {
 		for (int docNum = 0; docNum < LuceneWriteIndexFromFile.getIndexReader().maxDoc(); docNum++) {
-			String id = LuceneWriteIndexFromFile.getIndexReader().document(docNum)
-					.get("nodeHandlerID");
+			String id = LuceneWriteIndexFromFile.getIndexReader().document(docNum).get("nodeHandlerID");
 			if (id.equals(iMethod.getHandleIdentifier())) {
 				return docNum;
 			}
 		}
 		return null;
 	}
-	
-	
-		
-	public static ArrayList<Score> search(String queryString) throws Exception
-    {
-		
-		
-    	ArrayList<Score> searchResults = new ArrayList<>();
-    	
-    	
-    	
-        //Create lucene searcher. It search over a single IndexReader.
+
+	public static ArrayList<Score> search(String queryString) throws Exception {
+
+		ArrayList<Score> searchResults = new ArrayList<>();
+
+		// Create lucene searcher. It search over a single IndexReader.
 //        IndexSearcher searcher = createSearcher();
-        
-        methodMap = new HashMap<String, IMethod>();
-		
-        System.out.println("indexPath:" +indexPath);
-        Directory dir = FSDirectory.open(Paths.get(indexPath));
-         
-        //It is an interface for accessing a point-in-time view of a lucene index
-        IndexReader reader = DirectoryReader.open(dir);
-        
-        for (int i = 0; i < reader.numDocs(); i++) {
-			String methodID = reader.document(i).get(
-					"nodeHandlerID");
+
+		methodMap = new HashMap<String, IMethod>();
+
+		System.out.println("indexPath:" + indexPath);
+		Directory dir = FSDirectory.open(Paths.get(indexPath));
+
+		// It is an interface for accessing a point-in-time view of a lucene index
+		IndexReader reader = DirectoryReader.open(dir);
+
+		for (int i = 0; i < reader.numDocs(); i++) {
+			String methodID = reader.document(i).get("nodeHandlerID");
 //			System.out.println("methodID is" +methodID);
-			
+
 //			if (methodID == null
 //					|| methodMap.containsKey(methodID))
 //				continue;
@@ -312,76 +293,72 @@ public class LuceneWriteIndexFromFile
 				methodMap.put(methodID, method);
 //				System.out.println("methodMap put working?" +methodMap);
 			}
-			
-        }
-         
-        //Index searcher
-        IndexSearcher searcher = new IndexSearcher(reader);
-        
-         
-        //Search indexed contents using search term
-        TopDocs foundDocs = searchInContent(queryString, searcher);
-         
-        //Total found documents
-        System.out.println("Total Results :: " + foundDocs.totalHits);
-        
-//        String answer = "Total Results :: " + foundDocs.totalHits;
-        
-        //Let's print out the path of files which have searched term
-        
-        for (ScoreDoc sd : foundDocs.scoreDocs)
-        {	
-        	Document doc = searcher.doc(sd.doc);
-        	Float similarity = normalize(sd.score);
-            
-        	IMethod member = methodMap.get(doc.get("nodeHandlerID"));
-        
-        	String member1 = member.getKey();
-        	String[] parts = member1.split(";");
-        	String part1 = parts[0];
-        	String packageName  = part1.substring(1);
-        	
-        	String[] classnamearray = packageName.split("/");
-        	String className = classnamearray[classnamearray.length-1];
-        	System.out.println(className);
-//           Method method1 = Method.getMethod((IMethod) member);	
-            
-            String nameMethod = member.toString();
-            String[] methodSplit = nameMethod.split("\\(");
-            String methodSplitName = methodSplit[0];
-        	
-            String methodName  = methodSplitName.substring(0);
-        	
-//          IMethod method1 = methodMap.get(doc.get("path"));
-            
-            Score s = new Score(similarity, member, packageName, className, member);
-            
-            searchResults.add(s);
-            
-        }  
-		
-		return searchResults;
-    }
-	
-	    private static TopDocs searchInContent(String textToFind, IndexSearcher searcher) throws Exception
-	    {
-	        //Create search query
-	        QueryParser qp = new QueryParser("contents", new StandardAnalyzer());
-	        textToFind = IRUtil.splitAndMergeCamelCase(textToFind);
-	        Query query = qp.parse(QueryParserUtil.escape(textToFind));
-	         
-	        //search the index
-	        TopDocs hits = searcher.search(query, Integer.MAX_VALUE);
-	        return hits;
-	    }
-	    
-	    public static Query getQuery() {
-			return query;
+
 		}
-	    
-	    private static Float normalize(float score) {
-	 			return (float) (1.0 - Math.pow(1.0 / (1.0 + 0.2 * score), 5));
-	 		}
-	    
-	    
+
+		// Index searcher
+		IndexSearcher searcher = new IndexSearcher(reader);
+
+		// Search indexed contents using search term
+		TopDocs foundDocs = searchInContent(queryString, searcher);
+
+		// Total found documents
+		System.out.println("Total Results :: " + foundDocs.totalHits);
+
+//        String answer = "Total Results :: " + foundDocs.totalHits;
+
+		// Let's print out the path of files which have searched term
+
+		for (ScoreDoc sd : foundDocs.scoreDocs) {
+			Document doc = searcher.doc(sd.doc);
+			Float similarity = normalize(sd.score);
+
+			IMethod member = methodMap.get(doc.get("nodeHandlerID"));
+
+			String member1 = member.getKey();
+			String[] parts = member1.split(";");
+			String part1 = parts[0];
+			String packageName = part1.substring(1);
+
+			String[] classnamearray = packageName.split("/");
+			String className = classnamearray[classnamearray.length - 1];
+			System.out.println(className);
+//           Method method1 = Method.getMethod((IMethod) member);	
+
+			String nameMethod = member.toString();
+			String[] methodSplit = nameMethod.split("\\(");
+			String methodSplitName = methodSplit[0];
+
+			String methodName = methodSplitName.substring(0);
+
+//          IMethod method1 = methodMap.get(doc.get("path"));
+
+			Score s = new Score(similarity, member, packageName, methodName, className, member);
+
+			searchResults.add(s);
+
+		}
+
+		return searchResults;
+	}
+
+	private static TopDocs searchInContent(String textToFind, IndexSearcher searcher) throws Exception {
+		// Create search query
+		QueryParser qp = new QueryParser("contents", new StandardAnalyzer());
+		textToFind = IRUtil.splitAndMergeCamelCase(textToFind);
+		Query query = qp.parse(QueryParserUtil.escape(textToFind));
+
+		// search the index
+		TopDocs hits = searcher.search(query, Integer.MAX_VALUE);
+		return hits;
+	}
+
+	public static Query getQuery() {
+		return query;
+	}
+
+	private static Float normalize(float score) {
+		return (float) (1.0 - Math.pow(1.0 / (1.0 + 0.2 * score), 5));
+	}
+
 }
--- src/org/plugin/eclias/views/EcliasView.java
+++ src/org/plugin/eclias/views/EcliasView.java
@@ -1,6 +1,5 @@
 package org.plugin.eclias.views;
 
-
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Event;
@@ -43,25 +42,22 @@ import java.util.ArrayList;
 import javax.inject.Inject;
 
 /**
- * This sample class demonstrates how to plug-in a new
- * workbench view. The view shows data obtained from the
- * model. The sample creates a dummy model on the fly,
- * but a real implementation would connect to the model
- * available either in this or another plug-in (e.g. the workspace).
- * The view is connected to the model using a content provider.
+ * This sample class demonstrates how to plug-in a new workbench view. The view
+ * shows data obtained from the model. The sample creates a dummy model on the
+ * fly, but a real implementation would connect to the model available either in
+ * this or another plug-in (e.g. the workspace). The view is connected to the
+ * model using a content provider.
  * <p>
- * The view uses a label provider to define how model
- * objects should be presented in the view. Each
- * view can present the same model objects using
- * different labels and icons, if needed. Alternatively,
- * a single label provider can be shared between views
- * in order to ensure that objects of the same type are
- * presented in the same way everywhere.
+ * The view uses a label provider to define how model objects should be
+ * presented in the view. Each view can present the same model objects using
+ * different labels and icons, if needed. Alternatively, a single label provider
+ * can be shared between views in order to ensure that objects of the same type
+ * are presented in the same way everywhere.
  * <p>
  */
 
 public class EcliasView extends ViewPart {
-	
+
 //	private static final String NO_FILTER_STRING = "[no filter]";
 
 	/**
@@ -69,8 +65,9 @@ public class EcliasView extends ViewPart {
 	 */
 	public static final String ID = "org.plugin.eclias.views.EcliasView";
 
-	@Inject IWorkbench workbench;
-	
+	@Inject
+	IWorkbench workbench;
+
 	private TableViewer viewer;
 
 	private Composite queryComposite;
@@ -82,28 +79,29 @@ public class EcliasView extends ViewPart {
 	private Shell shell;
 	private Table table;
 
-	//private MethodListWidget resultList;
+	// private MethodListWidget resultList;
 	private Label resultsLabel;
 	private MethodListWidget resultList;
 	private MessageBox messageBox;
 	private TableItem item;
-	 
 
 	class ViewLabelProvider extends LabelProvider implements ITableLabelProvider {
 		@Override
 		public String getColumnText(Object obj, int index) {
 			return getText(obj);
 		}
+
 		@Override
 		public Image getColumnImage(Object obj, int index) {
 			return getImage(obj);
 		}
+
 		@Override
 		public Image getImage(Object obj) {
 			return workbench.getSharedImages().getImage(ISharedImages.IMG_OBJ_ELEMENT);
 		}
 	}
-	
+
 //	class NameSorter extends ViewerSorter {
 //	}
 
@@ -114,12 +112,11 @@ public class EcliasView extends ViewPart {
 
 	}
 
-	
 	@Override
 	public void createPartControl(Composite parent) {
-		
+
 		viewer = new TableViewer(parent, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL);
-		
+
 		viewer.setContentProvider(ArrayContentProvider.getInstance());
 		viewer.setInput(new String[] { "eCLIAS Tool" });
 		viewer.setLabelProvider(new ViewLabelProvider());
@@ -133,35 +130,32 @@ public class EcliasView extends ViewPart {
 //		contributeToActionBars();
 		shell = new Shell();
 		messageBox = new MessageBox(shell);
-		
+
 		GridLayout gridLayout = new GridLayout();
 		gridLayout.numColumns = 1;
 		parent.setLayout(gridLayout);
 
 		queryComposite = new Composite(parent, SWT.PUSH);
-		GridData queryCompositeGridData = new GridData(SWT.FILL, SWT.BEGINNING, true,
-				false);
+		GridData queryCompositeGridData = new GridData(SWT.FILL, SWT.BEGINNING, true, false);
 		queryComposite.setLayoutData(queryCompositeGridData);
 		GridLayout queryCompositeLayout = new GridLayout();
 		queryCompositeLayout.numColumns = 3;
 		queryCompositeLayout.marginWidth = 0;
 		queryComposite.setLayout(queryCompositeLayout);
-		
+
 		queryLabel = new Label(queryComposite, SWT.PUSH);
 		queryLabel.setText("Search Text:");
-		GridData queryLabelGridData = new GridData(SWT.LEFT, SWT.BOTTOM, true,
-				false);
+		GridData queryLabelGridData = new GridData(SWT.LEFT, SWT.BOTTOM, true, false);
 		queryLabel.setLayoutData(queryLabelGridData);
 		searchButton = new Button(queryComposite, SWT.PUSH);
 		searchButton.setText("Search");
-		GridData searchButtonGridData = new GridData(SWT.END, SWT.END, false,
-				false);
+		GridData searchButtonGridData = new GridData(SWT.END, SWT.END, false, false);
 		searchButton.setLayoutData(searchButtonGridData);
 		searchButton.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
 //				startSearch();
-				
+
 				try {
 					LuceneWriteIndexFromFile.index();
 					ArrayList<Score> s = LuceneWriteIndexFromFile.search(queryText.getText());
@@ -172,44 +166,67 @@ public class EcliasView extends ViewPart {
 //					mcp.main(null);
 //					System.out.println("Preprocessing Generated");
 //					MessageDialog.openInformation(shell, "Eclias", "Corpus Extracted and Indexed for the following projects:");
-					
-					for(Score sc : s) {
-						  item = new TableItem(table, SWT.NONE);
-						  item.setText(3, sc.getPackageName()+"");
-						  item.setText(1, sc.getMethodName()+"");
-					      item.setText(2, sc.getScore()+"");
-					      item.setText(0, sc.getMethod1()+"");
+
+					for (Score sc : s) {
+						item = new TableItem(table, SWT.NONE);
+						item.setText(0, sc.getMethodName() + "");
+						item.setText(1, sc.getClassName() + "");
+						item.setText(2, sc.getScore() + "");
+						item.setText(3, sc.getPackageName() + "");
+						item.setText(4, sc.getMethod() + "");
+						
+						table.addSelectionListener(new SelectionAdapter() {
+							
+							public void widgetSelected(SelectionEvent e) {
+								
+								System.out.println("edata"+e.item);
+								
+								
+								String[] tableclicked = e.item.toString().split("\\{");
+								String[] clicked = tableclicked[1].split("\\}");
+								String pathname = clicked[0];
+//								System.out.println(clicked[0]);
+//								System.out.println("pathname = " + pathname);
+//						          setFocus();
+								
+//								try {
+//										String methodname = sc.getMethodName();
+//										System.out.println("methodname = " + pathname);
+//										System.out.println("methodname = " + methodname);
+//										while(pathname != methodname) {
+//											System.out.println("methodname inside coming? ");
+//											(new RevealInEditorAction(sc.getMethod())).run();
+//											break;
+//										}
+//										
+//										System.out.println("its not breaking");
+//									}
+//
+//								catch (Exception e1) {
+//									// TODO Auto-generated catch block
+//									e1.printStackTrace();
+//								}
+
+							}
+							
+						});
 					}
 					
-					table.addSelectionListener(new SelectionAdapter() {
-					      public void widgetSelected(SelectionEvent e) {
-					    	  String[] tableclicked = e.item.toString().split("\\{");
-					    	  String[] clicked = tableclicked[1].split("\\}");
-//					    	  String pathname = clicked[0];
-					          System.out.println(clicked[0]);
-					          showMessage("Go to this method:\n" +clicked[0]);
-					          setFocus();
-					          
-//					          (new RevealInEditorAction(sc.getMethod1())).run();
-							   
-					      }
-					    });
 //				}
 //					queryText1.setText("hello");
 
 //					resultList.setText(s);
-					
+
 				} catch (Exception e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
 			}
 		});
-		
+
 		clearButton = new Button(queryComposite, SWT.PUSH);
 		clearButton.setText("Clear");
-		GridData clearButtonGridData = new GridData(SWT.END, SWT.END, false,
-				false);
+		GridData clearButtonGridData = new GridData(SWT.END, SWT.END, false, false);
 		clearButton.setLayoutData(clearButtonGridData);
 		clearButton.addSelectionListener(new SelectionAdapter() {
 			@Override
@@ -219,61 +236,49 @@ public class EcliasView extends ViewPart {
 			}
 		});
 
-		queryText = new StyledText(parent, SWT.BORDER | SWT.MULTI
-				| SWT.V_SCROLL | SWT.WRAP);
+		queryText = new StyledText(parent, SWT.BORDER | SWT.MULTI | SWT.V_SCROLL | SWT.WRAP);
 		queryText.setEditable(true);
-		GridData queryGridData = new GridData(SWT.FILL, SWT.BEGINNING, true,
-				false);
+		GridData queryGridData = new GridData(SWT.FILL, SWT.BEGINNING, true, false);
 		queryGridData.heightHint = 50;
 		queryText.setLayoutData(queryGridData);
 		queryText.setAlwaysShowScrollBars(false);
 
 		queryText.setText("");
-		
+
 		resultsLabel = new Label(parent, SWT.PUSH);
 		resultsLabel.setText("Results:");
 		resultsLabel.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-		
-		
-		table = new Table(parent, SWT.MULTI | SWT.BORDER | SWT.FULL_SELECTION | SWT.V_SCROLL
-		        | SWT.H_SCROLL);
-	    table.setLinesVisible(true);
-	    table.setHeaderVisible(true);
-		GridData queryGridData1 = new GridData(SWT.FILL, SWT.BEGINNING, true,
-				false);
+
+		table = new Table(parent, SWT.MULTI | SWT.BORDER | SWT.FULL_SELECTION | SWT.V_SCROLL | SWT.H_SCROLL);
+		table.setLinesVisible(true);
+		table.setHeaderVisible(true);
+		GridData queryGridData1 = new GridData(SWT.FILL, SWT.BEGINNING, true, false);
 		queryGridData1.heightHint = 100;
 		table.setLayoutData(queryGridData1);
-		
-//	    table.setBounds(250, 250, 220, 200);
-	    
-	    String[] titles = {"Method Name", "Score", "Package Name", "Full Path"};
-	    for (int i = 0; i < titles.length; i++) {
-	      TableColumn column = new TableColumn(table, SWT.NONE);
-	      column.setText(titles[i]);
-	    }
-	    
-	    for (int i=0; i<titles.length; i++) {
-	      table.getColumn (i).pack();
-	    }
-	    
-	    table.setSize(table.computeSize(SWT.DEFAULT, 200));
-	    
-	    
-	   
-	}
 
+//	    table.setBounds(250, 250, 220, 200);
+
+		String[] titles = { "Method Name", "Class Name", "Score", "Package Name", "Full Path" };
+		for (int i = 0; i < titles.length; i++) {
+			TableColumn column = new TableColumn(table, SWT.NONE);
+			column.setText(titles[i]);
+		}
+
+		for (int i = 0; i < titles.length; i++) {
+			table.getColumn(i).pack();
+		}
+
+		table.setSize(table.computeSize(SWT.DEFAULT, 200));
+
+	}
 
 	private void showMessage(String message) {
-		MessageDialog.openInformation(
-			viewer.getControl().getShell(),
-			"Eclias Tool",
-			message);
+		MessageDialog.openInformation(viewer.getControl().getShell(), "Eclias Tool", message);
 	}
-	
+
 	@Override
 	public void setFocus() {
 		viewer.getControl().setFocus();
 	}
-		
 
 }
