a0fff442fd0e804ff287d7b1fdc840affb546284
Vasanthgeethan Raju <vsanthgeethan.raju@gmail.com>
2020-01-17 16:31:07 -0700

    replaced folder name, changed the saving location, and auto populated the repo address

---
+++ src/org/plugin/eclias/goldSetsGeneratorFromSVNCommits/CorpusMethod.java
@@ -0,0 +1,26 @@
+package org.plugin.eclias.goldSetsGeneratorFromSVNCommits;
+
+public class CorpusMethod
+{
+	String methodID;
+	String methodContent;
+
+	public CorpusMethod(String methodID,String methodContent)
+	{
+		this.methodID=methodID;
+		this.methodContent=methodContent;
+	}
+
+	public boolean equals(Object corpusMethod)
+	{
+		CorpusMethod otherCorpusMethod=(CorpusMethod)corpusMethod;
+		if (this.methodID.equals(otherCorpusMethod.methodID))
+			return true;
+		return false;
+	}
+
+	public String toString()
+	{
+		return methodID+"\r\n"+methodContent+"\r\n";
+	}
+}
